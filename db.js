ca5d4699f76db2232ce04918becac80451bf5234
KrolLew23 <aniabandola@gmail.com>
2024-07-10 10:20:17 +0200

    update

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURI = 'mongodb+srv://aniabandola:aniagoitmongo@cluster0.iwtcnnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Database connection successful');
   } catch (error) {
     console.error('Database connection error:', error.message);
