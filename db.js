8d7d279faf34a4ee4bfcd78dc3d467e61623f7ec
KrolLew23 <aniabandola@gmail.com>
2024-07-10 09:45:18 +0200

    db

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://aniabandola:aniagoitmongo@cluster0.iwtcnnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Database connection successful');
+  } catch (error) {
+    console.error('Database connection error:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
